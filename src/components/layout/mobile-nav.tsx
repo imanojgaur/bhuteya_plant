f7d1f502463007363006c109fb0d931dd62aff98
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-03-14 12:52:38 +0000

    feat: complete initial header and navbar structure

--- src/components/layout/mobile-nav.tsx
+++ src/components/layout/mobile-nav.tsx
@@ -1,9 +1,9 @@
 import { ResponsiveDrawer } from "../shared/responsive-drawer";
 
 export default function MobileNav() {
-    return (
-        <div>
-            <ResponsiveDrawer />
-        </div>
-    );
+	return (
+		<div>
+			<ResponsiveDrawer />
+		</div>
+	);
 }
