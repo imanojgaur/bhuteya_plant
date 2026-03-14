5ae7af4bb9d8bc8c05772526445c32f06f625c03
Manoj Gaur <128888244+imanojgaur@users.noreply.github.com>
2026-03-14 12:32:11 +0000

    refactor: add more routes

--- src/lib/constants/routes.ts
+++ src/lib/constants/routes.ts
@@ -1,11 +1,17 @@
 export const ROUTES = {
-  HOME: "/",
-  TRENDING: "/trending",
-  CATEGORIES: {
-    SIZE: "/categories/size",
-    SPACE: "/categories/space",
-  },
-  SERVICES: {
-    BOOKING: "/services/maintenance-booking",
-  }
-} as const; 
+	HOME: "/",
+
+	TRENDING: "/trending",
+
+	CATEGORIES: {
+		ROOT: "/categories",
+		SIZE: "/categories/size",
+		SPACE: "/categories/space",
+	},
+	SERVICES: {
+		ROOT: "/categories",
+		BULK_INSTALLATION: "/services/bulk-installation",
+		CONSULT: "/services/consult-doctor",
+		MAINTENANCE: "/services/maintainance",
+	},
+} as const;
